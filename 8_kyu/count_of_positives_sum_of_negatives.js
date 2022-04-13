f5b440d62190e75f04c7c44465b9b30360044c8a
Sergio Mendez <sergio.mendez.roca@gmail.com>
2022-04-12 18:16:44 -0700

    Added coding challenge for 4-12-22

---
+++ 8_kyu/count_of_positives_sum_of_negatives.js
@@ -0,0 +1,29 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]
+
+
+
+
+function countPositivesSumNegatives(input) {
+    let newArr = []
+    let positive = 0
+    let negative = 0
+    if(input && input.length){
+      for(let i=0; i < input.length; i++){
+        if(input[i] > 0){
+          positive +=1
+        }
+        else{
+          negative += input[i]
+        }
+      }
+      newArr.push(positive)
+      newArr.push(negative)
+    }
+   return newArr
+ }
