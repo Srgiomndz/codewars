0e1db01db45e81606e43989bc94856b66402955c
Sergio Mendez <sergio.mendez.roca@gmail.com>
2022-04-17 12:01:39 -0700

    Added coding challenge for 4-17-22

---
+++ 8_kyu/name_shuffler.js
@@ -0,0 +1,13 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
+  
+  
+  
+  nameShuffler('Sergio Mendez')
+
+
+  
